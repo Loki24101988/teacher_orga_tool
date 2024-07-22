f7a3e9500b33884e906f3279e43116fa65d3c488
Raphael Sues <phalandra.mandragora@googlemail.com>
2024-07-22 07:03:51 +0200

    Ignore all Pupils in Json for a schoolclass

--- src/main/java/com/example/demo/pojos/Schoolclass.java
+++ src/main/java/com/example/demo/pojos/Schoolclass.java
@@ -2,18 +2,17 @@ package com.example.demo.pojos;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,6 +39,7 @@ public class Schoolclass {
 	@JoinColumn(name = "schoolclass_teacher_id", referencedColumnName = "teacherId")
 	private Teacher schoolclassTeacher;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "schoolclass")
 	private List<Pupil> pupils;
 }
