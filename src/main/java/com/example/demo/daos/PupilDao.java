28021faa88c66206715fa584a1df68d0fd8cd4cd
Raphael Sues <phalandra.mandragora@googlemail.com>
2024-07-24 00:38:13 +0200

    add Notes as List to Dao and show it on webpage

--- src/main/java/com/example/demo/daos/PupilDao.java
+++ src/main/java/com/example/demo/daos/PupilDao.java
@@ -1,5 +1,9 @@
 package com.example.demo.daos;
 
+import java.util.List;
+
+import com.example.demo.pojos.Note;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,4 +16,5 @@ public class PupilDao {
 	private String firstName;
 	private String lastName;
 	private String pupilId;
+	private List<Note> notes;
 }
