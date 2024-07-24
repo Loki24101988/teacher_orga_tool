1c5150d2dbe2ccbb513d28cba740c57cc36ba492
Raphael Sues <phalandra.mandragora@googlemail.com>
2024-07-24 19:10:46 +0200

    Added biolerplate for framework

--- src/main/java/com/example/demo/pojos/Teacher.java
+++ src/main/java/com/example/demo/pojos/Teacher.java
@@ -10,8 +10,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Teacher {
 
 	@Id
