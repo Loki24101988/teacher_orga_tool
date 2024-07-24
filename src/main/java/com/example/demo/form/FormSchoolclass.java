9caf97ad72aa3ddb522f86ae6be2be20de422701
Raphael Sues <phalandra.mandragora@googlemail.com>
2024-07-24 18:41:59 +0200

    create new methods to add and remove schoolclasses to database

---
+++ src/main/java/com/example/demo/form/FormSchoolclass.java
@@ -0,0 +1,13 @@
+package com.example.demo.form;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FormSchoolclass {
+
+	private String schoolclassName;
+}
--- src/main/java/com/example/demo/webside/controller/SchoolclassController.java
+++ src/main/java/com/example/demo/webside/controller/SchoolclassController.java
@@ -6,10 +6,13 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.converter.SchoolclassOverviewConverter;
 import com.example.demo.daos.SchoolclassOverviewDao;
+import com.example.demo.form.FormSchoolclass;
 import com.example.demo.pojos.Schoolclass;
 import com.example.demo.services.SchoolclassService;
 
@@ -31,7 +34,15 @@ public class SchoolclassController {
 		
 		List<Schoolclass> allSchoolclasses = this.schoolclassService.getAllSchoolclasses();
 		List<SchoolclassOverviewDao> collect = allSchoolclasses.stream().map(schoolclass -> this.converter.convert(schoolclass)).collect(Collectors.toList());
-		model.addAttribute("schoolclasses", collect);
+		model.addAttribute("schoolclassesDao", collect);
+		model.addAttribute("formSchoolclass", new FormSchoolclass());
 		return "schoolclassesOverview";
+	}
+	
+	@PostMapping("/addschoolclass")
+	public String addSchoolclassToDatabase(@ModelAttribute("formSchoolclass") FormSchoolclass formSchoolclass) {
+		Schoolclass createSchoolclass = this.schoolclassService.createSchoolclass(formSchoolclass.getSchoolclassName());
+		
+		return "redirect:allschoolclasses";
 	}
 }
