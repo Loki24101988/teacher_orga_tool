548bca986fe6c193d24f868a6b465a9ec8ef9cee
Raphael Sues <phalandra.mandragora@googlemail.com>
2024-07-24 18:42:50 +0200

    added new way to get to pupilOverview page, without button and added schoolclass to PupilDao, and added builder pattern to PupilDao

--- src/main/java/com/example/demo/converter/PupilConverter.java
+++ src/main/java/com/example/demo/converter/PupilConverter.java
@@ -11,7 +11,20 @@ public class PupilConverter implements Converter<Pupil, PupilDao>{
 
 	@Override
 	public PupilDao convert(Pupil source) {
-		return new PupilDao(source.getFirstName(), source.getLastName(), source.getPupilId(), source.getNotes());
+		String schoolclassName = "";
+		if(source.getSchoolclass() == null) {
+			schoolclassName = "no class set yet";
+		} else {
+			schoolclassName = source.getSchoolclass().getSchoolclassName();
+		}
+		return PupilDao
+				.builder()
+				.firstName(source.getFirstName())
+				.lastName(source.getLastName())
+				.schoolclassName(schoolclassName)
+				.pupilId(source.getPupilId())
+				.notes(source.getNotes())
+				.build();
 	}
 
 
--- src/main/java/com/example/demo/daos/PupilDao.java
+++ src/main/java/com/example/demo/daos/PupilDao.java
@@ -5,16 +5,19 @@ import java.util.List;
 import com.example.demo.pojos.Note;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class PupilDao {
 
 	private String firstName;
 	private String lastName;
 	private String pupilId;
 	private List<Note> notes;
+	private String schoolclassName;
 }
