1071c736e508756eda43d7a1141911d32b0325e9
Raphael Sues <phalandra.mandragora@googlemail.com>
2024-07-25 08:53:22 +0200

    added logic forhandle FormNoteForPupil class

--- src/main/java/com/example/demo/services/NoteService.java
+++ src/main/java/com/example/demo/services/NoteService.java
@@ -2,6 +2,9 @@ package com.example.demo.services;
 
 import java.util.List;
 
+import com.example.demo.exceptions.PupilNotfoundForIdException;
+import com.example.demo.exceptions.TeacherNotFoundException;
+import com.example.demo.form.FormNoteForPupil;
 import com.example.demo.pojos.Note;
 import com.example.demo.pojos.Pupil;
 import com.example.demo.pojos.Teacher;
@@ -12,4 +15,6 @@ public interface NoteService {
 	List<Note> getAllNotesForTeacher(Teacher teacher);
 	
 	Note storeNote(Note note);
+	
+	Note storeNote(FormNoteForPupil formNote) throws PupilNotfoundForIdException, TeacherNotFoundException;
 }
--- src/main/java/com/example/demo/services/NoteServiceImpl.java
+++ src/main/java/com/example/demo/services/NoteServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.demo.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exceptions.PupilNotfoundForIdException;
+import com.example.demo.exceptions.TeacherNotFoundException;
+import com.example.demo.form.FormNoteForPupil;
 import com.example.demo.pojos.Note;
 import com.example.demo.pojos.Pupil;
 import com.example.demo.pojos.Teacher;
@@ -14,8 +18,16 @@ public class NoteServiceImpl implements NoteService {
 
 	private NoteRepository noteRepository;
 	
-	public NoteServiceImpl(NoteRepository noteRepository) {
+	private PupilService pupilService;
+	
+	private TeacherService teacherService;
+	
+	public NoteServiceImpl(NoteRepository noteRepository, 
+			PupilService pupilService, 
+			TeacherService teacherService) {
 		this.noteRepository = noteRepository;
+		this.pupilService = pupilService;
+		this.teacherService = teacherService;
 	}
 
 	@Override
@@ -33,6 +45,18 @@ public class NoteServiceImpl implements NoteService {
 	@Override
 	public Note storeNote(Note note) {
 		Note save = this.noteRepository.save(note);
+		return save;
+	}
+
+	@Override
+	public Note storeNote(FormNoteForPupil formNote) throws PupilNotfoundForIdException, TeacherNotFoundException {
+		Note newNote = Note.builder()
+		.receiver(this.pupilService.getPupilForId(formNote.getPupilId()))
+		.creator(this.teacherService.getTeacherForIs(formNote.getTeacherId()))
+		.content(formNote.getContent())
+		.createdAt(LocalDateTime.now())
+		.build();
+		Note save = this.noteRepository.save(newNote);
 		return save;
 	}
 	
